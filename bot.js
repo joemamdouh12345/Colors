b7c63c83c841bf6a1d838c1e61001678b2dccf0d
admiin2bot <46852795+admiin2bot@users.noreply.github.com>
2019-01-23 03:10:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -472,9 +472,65 @@ client.on('ready',async () => {
 
 
 
-/////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////كود الالوان//////////////////////////////////
 
 
+ 
+  client.on('message', msg => {//msg
+    if (msg.content === `الالوان`) {
+      msg.channel.send({file : "https://cdn.discordapp.com/attachments/502797280332480512/537407353658671114/colors.png"})
+    }
+  });;
+
+
+
+
+  client.on('message', msg => {//msg
+    if (msg.content === `الوان`) {
+      msg.channel.send({file : "https://cdn.discordapp.com/attachments/502797280332480512/537407353658671114/colors.png"})
+    }
+  });;
+
+
+ 
+  client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+    if(message.content.split(' ')[0] == `لون`){
+    const embedd = new Discord.RichEmbed()
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+    .setDescription(`**لا يوجد لون بهذا الأسم ** :x: `)
+    .setColor(`ff0000`)
+   
+    if(!isNaN(args) && args.length > 0)
+   
+   
+    if    (!(message.guild.roles.find("name",`${args}`))) return  message.channel.sendEmbed(embedd);
+   
+   
+    var a = message.guild.roles.find("name",`${args}`)
+     if(!a)return;
+    const embed = new Discord.RichEmbed()
+   
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+    .setDescription(`**Done , تم تغير لونك . :white_check_mark: **`)
+   
+    .setColor(`${a.hexColor}`)
+    message.channel.sendEmbed(embed);
+    if (!args)return;
+    setInterval(function(){})
+       let count = 0;
+       let ecount = 0;
+    for(let x = 1; x < 201; x++){
+   
+    message.member.removeRole(message.guild.roles.find("name",`${x}`))
+   
+    }
+     message.member.addRole(message.guild.roles.find("name",`${args}`));
+   
+   
+    }
+    });
 
 
 /////////////////////////////////////////////////////////////////////
