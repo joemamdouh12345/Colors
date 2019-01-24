9143612aa6558efa98e71d5b31c23399d57a6749
admiin2bot <46852795+admiin2bot@users.noreply.github.com>
2019-01-24 14:36:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -535,7 +535,17 @@ client.on('ready',async () => {
 
 /////////////////////////////////////////////////////////////////////
 
-
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('http://www.imgion.com/images/01/Welcome-buddy.jpg')
+var channel =member.guild.channels.find('name', 'ღ══ωɛʟḉȏмε══ღ')
+if (!channel) return;
+channel.send({embed : embed});
+});
 
 
 /////////////////////////////////////////////////////////////////////
